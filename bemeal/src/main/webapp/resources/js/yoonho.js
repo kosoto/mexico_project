c6b817a21969e0f4110c769013dd850426141c9e
yoonho <kidwisps@naver.com>
2018-10-18 17:31:04 +0900

    1730

--- bemeal/src/main/webapp/resources/js/yoonho.js
+++ bemeal/src/main/webapp/resources/js/yoonho.js
@@ -5,19 +5,8 @@ yoonho.service=(()=>{
 	var list =x=>{
  		$('header').remove();
 		$('#content').empty();
-		alert('여기에 list넣자!')
-/*		내거 사용할 것.
- 		let $list = $('<div/>');
-		$.getScript($.script()+"/ui/y_item_detail.js").done(()=>{
-			for(let i = 1; i<4 ; i++){
-					$('<div/>').html(y_item_detailUI(i)).appendTo('#content')
-			}
-			
-		})*/
-		
-
 		let $res;
-		let $list =$('<div/>');
+		let $list=$('<div/>');
 		let titleList=['가장 평점이 높은','20대 구매율이 높은','신제품','베스트셀러','당신이 좋아할 만한 제품(login시?)/ 추천제품']
 		$.getJSON($.ctx()+'/item/list/first',d=>{
 			$.getScript($.script()+"/bemeal.js").done(()=>{
@@ -31,18 +20,51 @@ yoonho.service=(()=>{
 						})
 					)
 				}
-
-				})
 			})
-		$res = $('<div/>').appendTo('#content').append($list)
-			
-/*		let $listUI ;
-		$.getScript($.script+'/ui/y_item_detail.js').done(()=>{
-			$listUI = $('<div/>').append(divListUI())
-		})*/
-	return $res;
+		})
+		let $content = $('#content');
+		$content.addClass('container')
+			.append($('<span/>').addClass('col-lg-6 mgt50 btn-group')
+				.append($('<div/>').addClass('dropdown')
+					.append($('<a/>').addClass('btn btn-warning dropdown-toggle').attr({'type':"button",id:"dropdownMenu",'data-toggle':"dropdown",'aria-haspopup':"true",'aria-expanded':"false"}).html('메뉴'))
+					.append($('<div/>').addClass('dropdown-menu dropdown-warning')
+						.append($('<div/>').addClass('dropdown-item').attr({id:'dd_btn1','role':'button'}).html('도시락'))
+						.append($('<div/>').addClass('dropdown-item').attr({id:'dd_btn2','role':'button'}).html('샐러드'))
+						.append($('<div/>').addClass('dropdown-item').attr({id:'dd_btn3','role':'button'}).html('보울 도시락'))
+						.append($('<div/>').addClass('dropdown-item').attr({id:'dd_btn4','role':'button'}).html('반찬'))
+					)
+				)
+			)
+		$list = $('<div/>').html(
+					$.getScript($.script()+'/ui/y_item_comment.js').done(()=>{
+						 y_item_commentUI()
+					})
+				)
+		
+		let $carousels = $list.appendTo($content);
+
+		let num = 4;
+		let titles = [];
+		let $window = $(window);
+				$window.scroll(e=>{
+					if(num<=10 && $window.scrollTop()+$window.height()+30>$(document).height()){
+						$.getJSON($.ctx()+"/item/list/scrollTest",d=>{
+							$carousels.append(
+									bemeal.compo.carousel({
+										id:'carousel'+num,
+										title:'scrollTest'+(num-3),
+										arr:d.list,
+										row_size:5
+									})
+							);
+							num++;
+						}); 
+					}
+				});//scroll event end
+		
 	};
 	var retrieve = x=>{
+		//yoonho.contain.modal()
 		$.magnificPopup.open({
 			closeBtnInside:true,
 			closeOnContentClick:false,
@@ -50,7 +72,6 @@ yoonho.service=(()=>{
 			fixedBgPos: true,
 			fixedContentPos:false,
 			items:{src:
-
 				yoonho.service.popup()
 			},
 			midClick:true,
@@ -66,132 +87,116 @@ yoonho.service=(()=>{
 	var popup =x=>{
 		
 		//component
+		
+		let $tag1 = $('<div/>').attr({style:'margin:10px;font-size:18px'}).addClass('badge orange').html('#고소')
+		let $tag2 = $('<div/>').attr({style:'margin:10px;font-size:18px'}).addClass('badge orange').html('#달콤')
+		let $tag3 = $('<div/>').attr({style:'margin:10px;font-size:18px'}).addClass('badge orange').html('#짭짤')
 
-						
-		let $star = $('<div/>')
-					.append(
-						$('<span/>').addClass('star-input')
-						.append(
-							$('<span/>').addClass('input')
-							.append($('<input type="radio" name="star-input" id="p1" value="1"/>').append($('<label for="p1"/>').html('1')))
-							.append($('<input type="radio" name="star-input" id="p2" value="2"/>').append($('<label for="p2"/>').html('2')))
-							.append($('<input type="radio" name="star-input" id="p3" value="3"/>').append($('<label for="p3"/>').html('3')))
-							.append($('<input type="radio" name="star-input" id="p4" value="4"/>').append($('<label for="p4"/>').html('4')))
-							.append($('<input type="radio" name="star-input" id="p5" value="5"/>').append($('<label for="p5"/>').html('5')))
-							.append($('<input type="radio" name="star-input" id="p6" value="6"/>').append($('<label for="p6"/>').html('6')))
-							.append($('<input type="radio" name="star-input" id="p7" value="7"/>').append($('<label for="p7"/>').html('7')))
-							.append($('<input type="radio" name="star-input" id="p8" value="8"/>').append($('<label for="p8"/>').html('8')))
-							.append($('<input type="radio" name="star-input" id="p9" value="9"/>').append($('<label for="p9"/>').html('9')))
-							.append($('<input type="radio" name="star-input" id="p10" value="10"/>').append($('<label for="p10"/>').html('10')))
-							)
-						.append($('<output for="star-input"/>').html('<b>0</b>점'))
-						)
-						
-		let $commentHead = $('<div/>')
-						.append($('<span/>').addClass('text-muted').html('27 Aug 2018'))
-						.append($('<h5/>').html('맛짱'))
 		
-		let $comment = $('<div/>').append($('<p/>').html('매화도시락 너무 비싸요 음식점에서 사먹는가격보다 비쌈..'))
-						.append($('<i role="button"/>').addClass('fa fa-thumbs-o-up'))
-						.append($('<a/>').addClass('btn btn-warning').html('신고'))				
 		
-		//modal main
-		let $div1 = $('<div/>').attr({id:'y_item_detailUI'}).addClass('container yh-mfp-wrap yh-white-popup');// class : container yh-mfp-wrap yh-white-popup
-		let $div2 = $('<div/>').addClass('mfp-container detail-main')//yh-mfp-container
+
+		//modal main  
+		let $div1 = $('<div/>').attr({id:'y_item_detailUI'}).addClass('container yh-mfp-wrap yh-white-popup');
+		let $div2 = $('<div/>').addClass('mfp-container detail-main')
 					.append($('<span/>').addClass('col')
 						.append($('<img src="/web/resources/img/yoonho/maehwa_chicken.jpg"/>').addClass('rounded y_img_popup')))
-						.append($('<div/>')
-							.append(
-									$.getScript("/web/resources/ui/y_item_detail.js",()=>{
-										for(let i = 1; i<4 ; i++){
-											$('<div/>').append(y_item_detailUI(i))
-										}
-									})
-							)
-							
-							/**/
-							//.append($('<div/>').addClass('mgt50-mgb50'))
-						)
-
 		.appendTo($div1);// div2 end
 		
 	
-		let $div3 = $('<div/>').addClass('mgt50-mgba text-left')//<p style="font-size:20px">
-						.appendTo($div2)
-							.append($('<div/>').addClass('col-lg-6 mgt50')
-								.append($('<div/>').addClass('col-lg-6 mgt50-mgb50')
-									.append($('<h2/>').html('추천 도시락'))
-										.append($('<div/>')//.addClass('btn-group yh')  : 세로로 뜬다.
-											.append($('<button data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"/>').attr({type:'button'}).addClass('btn btn-warning dropdown-toggle').html('카테고리'))
-											.append($('<div/>').addClass('dropdown-menu')
-													.append($('<a/>').addClass('dropdown-item').attr({href:'#'}).html('같은 종류의 도시락'))
-													.append($('<a/>').addClass('dropdown-item').attr({href:'#'}).html('20대가 많이 산 도시락'))
-													.append($('<a/>').addClass('dropdown-item').attr({href:'#'}).html('이달의 도시락'))
-												)
-											
-										)
-									))//span end
-						;
+		let $div3 = $('<div/>').addClass('text-left')
+						.appendTo($div2);
 		
 		let $div4 = $('<div/>').addClass('mg0a').appendTo($div3);
-		let $div5 = $('<div/>').addClass('col-lg-6')
-								.append($('<button/>').html('장바구니').addClass('btn btn-warning mgt10-mgb10 mgr10-mgr10'))
-								.append($('<button/>').html('맛있어요').addClass('btn btn-warning mgt10-mgb10 mgr10-mgr10'))
-							.appendTo($div4)
+		let $div5 = $('<div/>').addClass('col-lg-10')
+								.append($('<a/>').html('장바구니').addClass('btn btn-warning mgt10-mgb10 mgr10-mgr10')
+										.click(e=>{
+											alert('장바구니 클릭!')
+											$.magnificPopup.close();//팝업창 끄는 효과 //우리는 멀티팝업 띄워야 함.
+										}))
+								.append($('<span/>').addClass('col-lg-6 mgt50 btn-group')
+									.append($('<div/>').addClass('dropdown')
+										.append($('<a/>').addClass('btn btn-warning dropdown-toggle').attr({'type':"button",id:"dropdownMenu",'data-toggle':"dropdown",'aria-haspopup':"true",'aria-expanded':"false"}).html('추천메뉴목록'))
+										.append($('<div/>').addClass('dropdown-menu dropdown-warning')
+											.append($('<div/>').addClass('dropdown-item').attr({id:'dropdown_btn1','role':'button'}).html('당신이 좋아할 만한 도시락'))
+											.append($('<div/>').addClass('dropdown-item').attr({id:'dropdown_btn2','role':'button'}).html('20대가 많이 구매한 도시락'))
+											.append($('<div/>').addClass('dropdown-item').attr({id:'dropdown_btn3','role':'button'}).html('신제품 도시락'))
+											.append($('<div/>').addClass('dropdown-item').attr({id:'dropdown_btn4','role':'button'}).html('베스트셀러'))
+										)
+									)
+								)
+								.append($('<a/>').attr({id:'detail_good_btn'}).html('맛있어요').addClass('btn btn-warning mgt10-mgb10 mgr10-mgr10')
+									.click(e=>{
+										alert('/맛있어서 자바갔다와야댐 $.ajax')
+									})
+								)
+							.appendTo($div4);
+		
+		let $div4_1 = $('<div/>').addClass('col-lg-8 mgt50-mgb100')
+						.appendTo($div3)
+							.append($('<div/>').html(yoonho.contain.carousel()))
+		
 		
 		//commentbox1,2
-		let $div4_1 = $('<div/>').addClass('col-lg-8 mgt50-mgb100').html('<h4>2개의 댓글이 작성되었습니다.</h4>').appendTo($div3);
+
+					
 		let $div5_1 = $('<div/>').addClass('single_comment_area').appendTo($div4_1);
-		let $span6_1 = $('<div/>').addClass('col-lg-8 mgt50-mgb50')// y_img_popup_div
-						.append( 
-							$('<img src="/web/resources/img/yoonho/img_avatar1.png"/>')
-							.addClass('rounded-circle y_img_popup_pr')
-							)
-							.appendTo($div5_1)
-							.append(
-									$commentHead
-									.append($star)
-									.append($comment)
-							);
-		
+		let $span6_1 = $('<div/>').addClass('col-lg-8 mgt50-mgb50').appendTo($div5_1)
+						.append(
+								$.getScript($.script()+"/ui/y_item_comment.js").done(()=>{
+									$('<div/>')
+									.html(y_item_cLstUI()).appendTo($div5_1)
+									.append(
+										$('<a role="button"/>').addClass('badge orange').attr({style:'margin:10px;font-size:18px','data-toggle':'modal','data-target':'#modalContactForm'}).html('수정').click(e=>{
+											alert('모달!!')
+											yoonho.contain.modal(y_item_cMdfUI());
+											
+										})
+									)
+									.append(
+										$('<a role="button"/>').addClass('badge orange').attr({style:'margin:10px;font-size:18px','data-toggle':'modal','data-target':'#modalContactForm'}).html('삭제').click(e=>{
+											alert('모달삭제!!')
+											yoonho.contain.modal(y_item_cDelUI());
+											
+										})
+									)
+								})
+						)
+						
 
 		let $span6_2 = $('<div/>').addClass('col-lg-8 mgt50-mgb50')// y_img_popup_div
-						.append( 
-							$('<img src="/web/resources/img/yoonho/img_avatar3.png"/>')
-							.addClass('rounded-circle y_img_popup_pr')
-							)
-							.appendTo($div5_1)
-							.append(
-									$commentHead
-									.append($star)
-									.append($comment)	
-							);
+						.append(
+								$.getScript($.script()+"/ui/y_item_comment.js").done(()=>{
+									$('<div/>').html(y_item_cWrtUI()).appendTo($div5_1)
+								})
+						).appendTo($div5_1);
 
 						
 							
 		//commentbox ed
-	
-							
-		//comment 작성페이지
-		let $div4_3 = $('<div/>').appendTo($div3);
-		let $div5_3 = $('<div/>').addClass('col-lg-8 mgt50-mgb100').append('<h3>Comment</h3>').appendTo($div4_3);
-		let $form6_3 = $('<form action="#" method="post"/>').appendTo($div5_3);
-		let $div7_3 = $('<div/>')
-		.append('<textarea textarea class="comment-testbox" name="message" id="message" cols="30" rows="10" placeholder="댓글을 입력하세요">')
-		.append($('<a/>').addClass('btn btn-warning').html('submit'))
-		.addClass('comment-testbox').appendTo($form6_3);
-		
 		let $div2_4 = $('<div/>').addClass('col-sm-5 mg0-pd0 detail-right yh-mfp-navi').appendTo($div1);
 		let $div3_4 = $('<div/>').addClass('container-fluid').appendTo($div2_4);
 		let $div4_4 = $('<div/>').addClass('row').appendTo($div3_4);
 		let $div5_4 = $('<div/>').addClass('col-lg-12 y_bg_popup_gr')
-						.append($('<h2/>').addClass('text-center').html('매화 (치킨, 연어구이)'))
+						.append($('<h2 style="font-size:28px;"/>').addClass('text-center font-weight-bold').html('매화 (치킨, 연어구이)'))
 						.append($('<p/>').addClass('text-center').html('사각 도시락'))
 						.appendTo($div4_4);
-		let $span6_4 = $star.appendTo($div5_4)
+		let $span6_4 = $('<div/>').html('<input id="input-1-rtl-star-xs" name="input-1-rtl-star-xs" class="kv-rtl-theme-svg-star rating-loading" value="1" dir="rtl" data-size="xs">'
+				+'<br/>'
+				+'<div class="clearfix"></div>').appendTo($div5_4)
 		
-			let $p6_4_1 = $('<p/>').addClass('text-left').html('1520명이 평가했어요').appendTo($div5_4)
-			let $p6_4_2 = $('<p role="button"/>').addClass('glyphicon glyphicon-thumbs-up').html('(743명)').appendTo($div5_4)
+			let $p6_4_1 = $('<p/>').addClass('text-left').html('1520명이 평가했어요')
+						.append($('<div/>')
+							.append($tag1)
+							.append($tag2)
+							.append($tag3))
+						.appendTo($div5_4)
+			let $p6_4_2 = $('<div/>').html('(743명)').appendTo($div5_4)
+							.append($('<div/>').html(
+								$.getScript($.script()+'/ui/y_item_comment.js').done(()=>{
+									//안뜸
+									y_item_cStarUI()
+								})
+							))
 			
 			let $div4_5 = $('<div/>').addClass('row').appendTo($div3_4)
 			let $div5_5 = $('<div/>').addClass('col-lg-12 y_bg_popup_bg').appendTo($div4_5)
@@ -214,14 +219,12 @@ yoonho.service=(()=>{
 			let $tbody7_8_2 = $('<div/>').append($('<img src="/web/resources/img/yoonho/allergy2.png">').addClass('rounded y_img_popup_alg')).appendTo($tbody7_8 )
 			let $tbody7_8_3 = $('<div/>').append($('<img src="/web/resources/img/yoonho/allergy3.png">').addClass('rounded y_img_popup_alg')).appendTo($tbody7_8 )
 			
-			let $tbl6_9 = $('<div/>').addClass('row').appendTo($div5_5)
+			let $tbl6_9 = $('<div/>').addClass('row')
+							.appendTo($div5_5)
 			let $tbody7_9 = $('<div/>').html('[베스트 & 스테디셀러 SINCE 2007] 12가지 다양한 반찬으로 구성된 프리미엄 도시락입니다. 푸짐한 반찬과 함께 촉촉하고 부드러운 연어구이와 치킨이 구성되어 있어 부족하지 않고 든든하게 드실 수 있는 도시락 입니다. 생수와 조미 김이 함께 제공 됩니다.').addClass('col-sm text-left mgt10-mgb10').appendTo($tbl6_9)
-			
+		
 		return $div1;
 	};
-	
-
-
 
 	return {
 		list:list,
@@ -230,6 +233,152 @@ yoonho.service=(()=>{
 	};
 	
 	
+})();
+
+
+yoonho.contain=(x=>{
+	var drop=x=>{
+		return '<span class="btn-group">'
+		+'  <!--Dropdown primary-->'
+		+'  <div class="dropdown">'
+		+''
+		+'    <!--Trigger-->'
+		+'    <a class="btn btn-warning dropdown-toggle" type="button" id="dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">추천메뉴목록</a>'
+		+''
+		+'    <!--Menu-->'
+		+'    <div class="dropdown-menu dropdown-warning">'
+		+'      <a class="dropdown-item" href="#">당신이 좋아할 만한 도시락</a>'
+		+'      <a class="dropdown-item" href="#">20대가 많이 구매한 도시락</a>'
+		+'      <a class="dropdown-item" href="#">신제품 도시락</a>'
+		+'      <a class="dropdown-item" href="#">베스트셀러</a>'
+		+'    </div>'
+		+'  </div>'
+		+'  <!--/Dropdown primary-->'
+		+'</span>'
+	}
+	var carousel=x=>{
+		return '  <!--Carousel Wrapper-->'
+		+'<div id="carousel-with-lb" class="carousel slide carousel-multi-item" data-ride="carousel">'
+		+''
+		+'  <!--Controls-->'
+		+'  <div class="text-center controls-top">'
+		+'    <a class="btn-floating btn-secondary" href="#carousel-with-lb" data-slide="prev"><i class="fa fa-chevron-left"></i></a>'
+		+'    <a class="btn-floating btn-secondary" href="#carousel-with-lb" data-slide="next"><i class="fa fa-chevron-right"></i></a>'
+		+'  </div>'
+		+'  <!--/.Controls-->'
+		+''
+		+'  <!--Indicators-->'
+		+'  <ol class="carousel-indicators">'
+		+'    <li data-target="#carousel-with-lb" data-slide-to="0" class="active secondary-color"></li>'
+		+'    <li data-target="#carousel-with-lb" data-slide-to="1" class="secondary-color"></li>'
+		+'    <li data-target="#carousel-with-lb" data-slide-to="2" class="secondary-color"></li>'
+		+'  </ol>'
+		+'  <!--/.Indicators-->'
+		+''
+		+'  <!--Slides and lightbox-->'
+		+''
+		+'  <div class="carousel-inner mdb-lightbox" role="listbox">'
+		+'    <div id="mdb-lightbox-ui"></div>'
+		+'    <!--First slide-->'
+		+'    <div class=" carousel-item active text-center">'
+		+''
+		+'      <figure class="col-md-2 d-md-inline-block">'
+		+'        <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(2).jpg" data-size="1600x1067">'
+		+'          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(2).jpg" class="img-fluid">'
+		+'        </a>'
+		+'      </figure>'
+		+''
+		+'      <figure class="col-md-2 d-md-inline-block">'
+		+'        <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(4).jpg" data-size="1600x1067">'
+		+'          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(4).jpg" class="img-fluid">'
+		+'        </a>'
+		+'      </figure>'
+		+''
+		+'      <figure class="col-md-2 d-md-inline-block">'
+		+'        <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(6).jpg" data-size="1600x1067">'
+		+'          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(6).jpg" class="img-fluid">'
+		+'        </a>'
+		+'      </figure>'
+		+''
+		+'    </div>'
+		+'    <!--/.First slide-->'
+		+''
+		+'    <!--Second slide-->'
+		+'    <div class="carousel-item text-center">'
+		+''
+		+'      <figure class="col-md-2 d-md-inline-block">'
+		+'        <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(22).jpg" data-size="1600x1067">'
+		+'          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(22).jpg" class="img-fluid">'
+		+'        </a>'
+		+'      </figure>'
+		+''
+		+'      <figure class="col-md-2 d-md-inline-block">'
+		+'        <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(25).jpg" data-size="1600x1067">'
+		+'          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(25).jpg" class="img-fluid">'
+		+'        </a>'
+		+'      </figure>'
+		+''
+		+'      <figure class="col-md-2 d-md-inline-block">'
+		+'        <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(29).jpg" data-size="1600x1067">'
+		+'          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(29).jpg" class="img-fluid">'
+		+'        </a>'
+		+'      </figure>'
+		+''
+		+'    </div>'
+		+'    <!--/.Second slide-->'
+		+''
+		+'    <!--Third slide-->'
+		+'    <div class="carousel-item text-center">'
+		+''
+		+'      <figure class="col-md-2 d-md-inline-block">'
+		+'        <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(44).jpg" data-size="1600x1067">'
+		+'          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(44).jpg" class="img-fluid">'
+		+'        </a>'
+		+'      </figure>'
+		+''
+		+'      <figure class="col-md-2 d-md-inline-block">'
+		+'        <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(45).jpg" data-size="1600x1067">'
+		+'          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(45).jpg" class="img-fluid">'
+		+'        </a>'
+		+'      </figure>'
+		+''
+		+'      <figure class="col-md-2 d-md-inline-block">'
+		+'        <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(66).jpg" data-size="1600x1067">'
+		+'          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(66).jpg" class="img-fluid">'
+		+'        </a>'
+		+'      </figure>'
+		+'    </div>'
+		+'    <!--/.Third slide-->'
+		+''
+		+'  </div>'
+		+'  <!--/.Slides-->'
+		+''
+		+'</div>'
+		+'<!--/.Carousel Wrapper-->'
+	}
+	var modal=x=>{
+		return $.magnificPopup.open({
+			closeBtnInside:true,
+			closeOnContentClick:false,
+			alignTop: true,
+			fixedBgPos:true,
+			fixedContentPos:false,
+			items:{src:
+				x
+			},
+			midClick:true,
+			overflowY:'auto',
+			removalDelay:'0',
+			type:'inline'}); 
+		$('.btn').on('click',function(){
+			
+			alert($('#code').val());
+		});
+	};
+	var layout=()=>{
+		
+	};
+	return {layout:layout,drop:drop,carousel:carousel,modal:modal};
 })();
 
 
