830d1619d7c5328355ea99d715341bbae0ae29b0
kstad <kstad@naver.com>
2018-10-23 16:51:51 +0900

    181023 16:51

--- bemeal/src/main/java/com/bemeal/web/taste/TasteMapper.java
+++ bemeal/src/main/java/com/bemeal/web/taste/TasteMapper.java
@@ -10,5 +10,10 @@ import org.springframework.stereotype.Repository;
 public interface TasteMapper {
 	public void post(Taste p);
 	public int existGrade(String id);
-	public ArrayList<Map<String, Object>> chart(String id);
+	public ArrayList<Map<String, Object>> chartArea(String id);
+	public ArrayList<Map<String, Object>> chartIngre();
+	public ArrayList<Map<String, Object>> chartBrand();
+	public ArrayList<Map<String, Object>> chartTaste();
+	public ArrayList<Map<String, Object>> chartEmotion(); 
+	public ArrayList<Map<String, Object>> chartMenu();
 }
