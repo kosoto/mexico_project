82057e7572282387dffc5f3b15c7b95507a3dc23
kstad <kstad@naver.com>
2018-10-26 17:29:17 +0900

    17:29

--- bemeal/src/main/java/com/bemeal/web/cmm/HomeCtrl.java
+++ bemeal/src/main/java/com/bemeal/web/cmm/HomeCtrl.java
@@ -10,18 +10,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.bemeal.web.dummy.DummyCtrl;
 
 
 @Controller
 public class HomeCtrl {
 	private static final Logger logger = LoggerFactory.getLogger(HomeCtrl.class);
-	@Autowired DummyCtrl ctrl;
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model, HttpServletRequest request) {
 		System.out.println("-------------------------------");
 		model.addAttribute("context", request.getContextPath());
-		ctrl.dummy();
 		return "main";
 	}
 }
