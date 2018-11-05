a25f536e1d120dcc6ccaebdfef0ad0a2104b60cb
kstad <kstad@naver.com>
2018-11-05 09:21:22 +0900

    09:21

--- bemeal/src/main/java/com/bemeal/web/search/SearchCtrl.java
+++ bemeal/src/main/java/com/bemeal/web/search/SearchCtrl.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-
 public class SearchCtrl {
 	private static final Logger logger = LoggerFactory.getLogger(SearchCtrl.class);
 	@Autowired HashMap<String,Object> map;
